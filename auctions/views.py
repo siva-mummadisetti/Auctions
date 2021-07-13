f3ff0ac4a8b0cbe7fa4c9b9542145043a6637823
siva-mummadisetti <75797330+siva-mummadisetti@users.noreply.github.com>
2021-07-13 19:57:04 +0530

    Update views.py

--- auctions/views.py
+++ auctions/views.py
@@ -74,10 +74,6 @@ def register(request):
             return render(request, "auctions/register.html", {
                 "message": "Username already taken."
             })
-        except ValueError:
-            return render(request, "auctions/register.html", {
-                "message": "Please fill all the fields."
-            })
         login(request, user)
         return HttpResponseRedirect(reverse("index"))
     else:
